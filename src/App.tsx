4d6efb0d75d4e50d7cce2c19c7654bc0768f96e9
Erick Massa Sprengel <erick.sprengel@gmail.com>
2025-10-16 10:44:47 -0300

    feat(table): fisrt tab table with actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from './components/ui/table';
+import { Button } from './components/ui/button';
+import { tabsMock } from './chrome/tabsMock';
+import { FocusIcon, Trash2Icon } from 'lucide-react';
+
+interface Tab {
+  id: number;
+  active: boolean;
+  audible: boolean;
+  autoDiscardable: boolean;
+  discarded: boolean;
+  favIconUrl?: string;
+  frozen: boolean;
+  groupId: number;
+  height: number;
+  highlighted: boolean;
+  lastAccessed: number;
+  mutedInfo: {
+    muted: boolean;
+  };
+  pinned: boolean;
+  selected: boolean;
+  splitViewId: number;
+  status: string;
+  title: string;
+  url: string;
+  width: number;
+  windowId: number;
+}
+
+interface TabsBanana extends Tab {
+  duplicated_tabs: Tab[];
+}
+
+const processTabs = (tabs: Tab[]): TabsBanana[] => {
+  return tabs.map((tab) => ({
+    ...tab,
+    duplicated_tabs: tabs.filter((t) => t.url === tab.url),
+  }));
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tabs, setTabs] = useState<TabsBanana[]>([])
+
+  const updateTabs = async () => {
+    // @ts-ignore
+    if (!chrome.tabs) {
+      setTabs(processTabs(tabsMock));
+      return;
+    }
+
+    // @ts-ignore
+    const tabsNew = await chrome.tabs.query({
+      url: ["<all_urls>"]
+    });
+    setTabs(processTabs(tabsNew));
+    console.log(tabs);
+  }
+
+  useEffect(() => {
+    updateTabs();
+  }, []);
+
+  const handleOnRemoveTab = async (tab: TabsBanana) => {
+    // @ts-ignore
+    await chrome.tabs.remove(tab.id);
+    updateTabs();
+  }
+
+  const handleOnRemoveDuplicatedTab = async (tab: TabsBanana) => {
+    // @ts-ignore
+    await chrome.tabs.remove(tab.duplicated_tabs.map((t) => t.id));
+    updateTabs();
+  }
+
+  const handleOnFocusTab = async (tab: TabsBanana) => {
+    // @ts-ignore
+    await chrome.tabs.update(tab.id, { active: true });
+    updateTabs();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div className="w-full h-full p-3">
+      <h1>Tabs</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Button onClick={updateTabs}>
+          count is {tabs.length}
+        </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Action</TableHead>
+          <TableHead className="w-[100px]">Link</TableHead>
+          <TableHead className="w-[100px]">Duplicated</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead>Last Accessed</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {tabs.map((tab) => (
+          <TableRow key={tab.id}>
+            <TableCell className="flex gap-2">
+              <Button onClick={() => handleOnRemoveTab(tab)}>
+                <Trash2Icon />
+              </Button>
+              <Button onClick={() => handleOnFocusTab(tab)}>
+                <FocusIcon />
+              </Button>
+            </TableCell>
+            <TableCell className="w-[100px]">
+              <Button onClick={() => handleOnRemoveDuplicatedTab(tab)}>
+                <Trash2Icon /> {tab.duplicated_tabs.length}
+              </Button>
+              </TableCell>
+            <TableCell className="font-medium">
+              {tab.url.substring(0, 30)}
+            </TableCell>
+            <TableCell className="line-clamp-1 max-w-[200px]">{tab.title}</TableCell>
+            <TableCell className="text-right">{new Date(tab.lastAccessed).toLocaleString()}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total Tabs</TableCell>
+          <TableCell className="text-right">{tabs.length}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+    </div>
   )
 }
 
