8f31445cb30734c940b4b7b00cdff38b932a2d6e
Erick Massa Sprengel <erick.sprengel@gmail.com>
2025-10-30 00:30:41 -0300

    feat(core): convert to side panel

--- public/background.js
+++ public/background.js
@@ -1,8 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
 
 chrome.commands.onCommand.addListener((command) => {
   console.log(`Command: ${command}`);
   if (command === "open-adhd-tab-manager") {
-    // open adhd extension popup
-    chrome.action.openPopup()
+    chrome.windows.getCurrent(window => {
+      chrome.sidePanel.open({ windowId: window.id }, () => console.log("Side panel opened"));
+    });
   }
 });
